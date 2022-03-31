89d98635596f1ada5811ff30a43c1e18f07b2919
marco cruz <marco24cruz08@gmail.com>
2022-03-30 22:13:03 -0600

    :tada: First commit

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 app.get('/', (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('💥 PUUUMMM 💥: ' + err);
+      res.status(500).send('💥 BUMMM 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('Me conecté a la DB..! 😎');
       db.close();
     }
   });
